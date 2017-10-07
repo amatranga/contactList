62a9a38254119ca6f67a8beece7f92b277c7429f
amatranga <amatranga892@gmail.com>
2017-10-07 14:27:15 -0700

    Implements skeleton components
    contact-detail-view
    contact-list-view
     --contact-item

---
+++ src/app/contact-detail-view/contact-detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-detail-view',
+  templateUrl: './contact-detail-view.component.html',
+  styleUrls: ['./contact-detail-view.component.css']
+})
+export class ContactDetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
