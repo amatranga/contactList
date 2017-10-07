eb0d6424f594d2c78eb863a6be4a23f3e2622950
amatranga <amatranga892@gmail.com>
2017-10-07 14:45:57 -0700

    Implements app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './shared/contact.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loadedFeature = 'contactList';
+  selectedContact: Contact;
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  handleContactSelection(contact) {
+    this.loadedFeature = 'detailView';
+    this.selectedContact = contact;
+  }
 }
