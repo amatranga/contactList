2cee0d9b2b8435a91004609f5216b2bd399f41b6
amatranga <amatranga892@gmail.com>
2017-10-07 14:33:49 -0700

    Implement navbar and shared models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContactDetailViewComponent } from './contact-detail-view/contact-detail-view.component';
 import { ContactListViewComponent } from './contact-list-view/contact-list-view.component';
 import { ContactItemComponent } from './contact-list-view/contact-item/contact-item.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailViewComponent,
     ContactListViewComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
